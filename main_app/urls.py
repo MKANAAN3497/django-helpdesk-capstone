86f79ad14971c6c88d5ff4b645625c32a4d8da5c
unknown <mostafakanan0@gmail.com>
2025-10-11 16:13:37 +0300

    update urls.py code

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
-urlpatterns = [ 
-     path('admin/', admin.site.urls), 
-     path('', include('name-of-your-application.urls')), 
-     ]
+from . import views
+
+urlpatterns = [
+    path('', views.ticket_list, name='ticket_list'),
+    path('tickets/new/', views.ticket_create, name='ticket_create'),
+    path('tickets/<int:pk>/', views.ticket_detail, name='ticket_detail'),
+    path('tickets/<int:pk>/edit/', views.ticket_update, name='ticket_update'),
+    path('tickets/<int:pk>/delete/', views.ticket_delete, name='ticket_delete'),
+
+    path('categories/', views.category_list, name='category_list'),
+    path('categories/new/', views.category_create, name='category_create'),
+]
