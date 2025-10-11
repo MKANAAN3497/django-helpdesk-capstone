21e72947c92c5fd1fc3645ffdda884ac5be42dfd
unknown <mostafakanan0@gmail.com>
2025-10-11 16:16:05 +0300

    main app admin.py code

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Category, Ticket
+admin.site.register(Category)
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('id','title','status','priority','category','created_by','assigned_to','updated_at')
+    list_filter = ('status','priority','category')
+    search_fields = ('title','description','created_by__username','assigned_to__username')
